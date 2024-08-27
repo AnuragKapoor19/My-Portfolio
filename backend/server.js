22eb5e8a9267e192124f95df96edf4c27abaa378
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2024-08-27 18:21:29 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const PORT = process.env.PORT || 5000
 const app = express()
 const path = require('path')
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../frontend/build')))
