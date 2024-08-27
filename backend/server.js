b1467cccdffb1d01e1958bb597b0a253c732e343
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2024-08-27 18:31:00 +0530

    Commit

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 const path = require('path')
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}))
+app.use(cors())
 
 app.use(express.json())
 
